92ef14b98f1f5da528d03804507994809dd3319e
Ishit-Parikh <ishitparikh25@gmail.com>
2026-06-28 18:39:09 +0530

    Added dynamic bounds to player movement limits based on the camera view

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,18 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float playerPadding = 0.5f;
+    [SerializeField] private float moveSpeed = 4.5f;
+    
+    public float moveLimitLeft, moveLimitRight;
+    
     void Start()
     {
-        
+        moveLimitLeft = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x + playerPadding;
+        moveLimitRight = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0)).x - playerPadding;
     }
-
-    [SerializeField] float moveLimitLeft = -4f; 
-    [SerializeField] float moveLimitRight = 4f;
     
     void Update()
     {
@@ -18,36 +20,27 @@ public class PlayerController : MonoBehaviour
 
         if (Keyboard.current.aKey.isPressed)
         {
-            if (transform.position.x <= moveLimitLeft)
-            {
-                move = 0f;
-            }
-            else
-            {
-                move = -7.5f;
-            }
+            move = -7.5f;
         }
         else if (Keyboard.current.dKey.isPressed)
-        {
-            if (transform.position.x >= moveLimitRight)
-            {
-                move = 0f;
-            }
-            else
-            {
-                move = 7.5f;
-            }
+        { 
+            move = 7.5f;
         }
         
         //translate will help direction of acceleration (Move i.e. left, right, up, down)
-        transform.Translate(move * Time.deltaTime, 0, 0);
+        transform.Translate(move * moveSpeed * Time.deltaTime, 0, 0);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, moveLimitLeft, moveLimitRight);
+        transform.position = pos;
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Obstacle"))
         {
-            Debug.Log("GAME-OVER!");
+            Debug.Log("GameOver");
         }
     }
 }
