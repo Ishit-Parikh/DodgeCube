e3c91632c464d99d115c9acb958a463800cf887e
Ishit-Parikh <ishitparikh25@gmail.com>
2026-06-29 15:51:42 +0530

    Made it so that this script only controlls player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,44 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float playerPadding = 0.5f;
-    [SerializeField] private float moveSpeed = 4.5f;
-    
-    public float moveLimitLeft, moveLimitRight;
+    [SerializeField] private float moveSpeed = 2.5f;
+    private GameManager _gameManager;
     
     void Start()
     {
-        moveLimitLeft = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x + playerPadding;
-        moveLimitRight = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0)).x - playerPadding;
+        _gameManager = Object.FindAnyObjectByType<GameManager>();
     }
     
     void Update()
     {
         float move = 0f;
-
         if (Keyboard.current.aKey.isPressed)
         {
             move = -7.5f;
         }
-        else if (Keyboard.current.dKey.isPressed)
-        { 
+        else if (Keyboard.current.dKey.isPressed) 
+        {
             move = 7.5f;
         }
-        
+            
         //translate will help direction of acceleration (Move i.e. left, right, up, down)
         transform.Translate(move * moveSpeed * Time.deltaTime, 0, 0);
 
-        Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, moveLimitLeft, moveLimitRight);
+        Vector3 pos = transform.position; 
+        pos.x = Mathf.Clamp(pos.x, _gameManager.moveLimitLeft, _gameManager.moveLimitRight);
         transform.position = pos;
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Obstacle"))
-        {
-            Debug.Log("GameOver");
-        }
     }
 }
