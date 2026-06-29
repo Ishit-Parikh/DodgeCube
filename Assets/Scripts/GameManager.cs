dc03c2917e6897c9372e21ee7e96b5698740131b
Ishit-Parikh <ishitparikh25@gmail.com>
2026-06-29 18:52:22 +0530

    Added Score Panel, Game Over Panel along with final score, restart button with score reset to zero

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,50 +1,80 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    //for setting player bounds that can restrict players movement based on screen Size
     public float moveLimitLeft, moveLimitRight, playerPadding = 0.5f;
-    
-    public PlayerController _playerController;
-    
-    public bool isGameActive = false;
-    public GameObject _mainMenuPanel;
-    
-    public GameObject droppyPrefab;
-    public bool isObstacleSpawned = false;
-    public float xPostion;
-    
-    
-    public GameObject boppyPrefab;
-    public bool isPlayerSpawned = false;
 
-    //public int score = 0;
-    //public TextMeshProUGUI ScoreText;
-    //public TextMeshProUGUI FinalSocreText;
-
-    /*public void updateScore()
-    {
-        ScoreText.text = "Score: " + score.ToString();
-    }*/
-
-    void Start()
-    {
-        moveLimitLeft = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x + playerPadding;
-        moveLimitRight = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0)).x - playerPadding;
-    }
-
+    //Variables and Methods that will help start the game
+    public bool isGameActive = false; 
     public void GameStart()
     {
         isGameActive = true;
+        scorePanel.SetActive(true);
     }
-
+    
+    
+    //Variables & Methods that will help spawn the player
+    public GameObject boppyPrefab;
+    public bool isPlayerSpawned;
+    
     private void SpawnPlayer()
     {
         isPlayerSpawned = true;
         Instantiate(boppyPrefab, new Vector3(0, -2.295f, 0), Quaternion.identity);
     }
     
-    // Update is called once per frame
+    
+    //Variables & Methods that will help manage the scoring system
+    public GameObject scorePanel;
+    public int score;
+    public TextMeshProUGUI ScoreText;
+    
+    public void updateScore()
+    {
+        ScoreText.text = "Score: " + score.ToString();
+    }
+    
+    
+    //Variables that will help in showing the final score after Game is over
+    public int finalScore;
+    public TextMeshProUGUI FinalSocreText;
+    public bool isGameOver;
+    public GameObject gameOverPanel;
+    
+    private void GameOverPanel()
+    {
+        scorePanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+        finalScore = score;
+        FinalSocreText.text = "Final Score: " + finalScore.ToString();
+        isGameOver = false;
+    }
+    
+    public void RestartGame()
+    {
+        score = 0;
+        updateScore();
+        finalScore = 0;
+        GameStart();
+    }
+    
+    void Start()
+    {
+        moveLimitLeft = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x + playerPadding;
+        moveLimitRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - playerPadding;
+        gameOverPanel.SetActive(false);
+        scorePanel.SetActive(false);
+    }
+    
+    //Variables that will help in spawining of Obstacles
+    public bool isObstacleSpawned = false;
+    public float xPostion;
+    public GameObject droppyPrefab;
+    
+    
     void Update()
     {
         if(isGameActive)
@@ -54,12 +84,16 @@ public class GameManager : MonoBehaviour
                 SpawnPlayer();
             }
             
-            if (!isObstacleSpawned)
+            else if (!isObstacleSpawned)
             {
                 xPostion = Random.Range(moveLimitLeft, moveLimitRight);
                 Instantiate(droppyPrefab, new Vector3(xPostion, 0, 0), Quaternion.identity);
                 isObstacleSpawned = true;
             }
+        }
+        else if (isGameOver)
+        {
+            GameOverPanel();
         }
     }
 }
